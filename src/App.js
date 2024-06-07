7f140686a26be1a4df3002ed77e66c354d687086
Marcel Neroba <marcel.neroba@transgourmet.de>
2024-06-07 14:12:11 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import SensorChart from "./components/LineChart";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [timeStamps, setTimeStamps] = useState([]);
+    const [sensor, setSensor] = useState([]);
+
+    const fetchData = () => {
+        fetch("http://localhost:8000/data")
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setTimeStamps(data.timestamps);
+                setSensor(data.sensor);
+            })
+            .catch((error) => {
+                console.error('There was a problem with the fetch operation:', error);
+            });
+    };
+    useEffect(() => {
+        fetchData();
+        const intervalId = setInterval(fetchData, 10000);
+        return () => clearInterval(intervalId);
+    }, []);
+
+
+    return (<>
+            <div>
+                <h2>Line Chart</h2>
+                <SensorChart sensor={sensor} timestamps={timeStamps}/>
+
+            </div>
+
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/LineChart.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { LineChart } from '@mui/x-charts/LineChart';
+
+export default function SensorChart({timestamps,sensor}) {
+    return (
+        <LineChart
+            xAxis={[{ data: timestamps }]}
+            series={[
+                {
+                    data: sensor,
+                },
+            ]}
+            height={300}
+            margin={{ left: 30, right: 30, top: 30, bottom: 30 }}
+            grid={{ vertical: true, horizontal: true }}
+        />
+    );
+}
